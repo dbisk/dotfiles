ec57919c9dc0a5a5f9abfce3817b6210cc9c8917
dbisk <16860792+dbisk@users.noreply.github.com>
2021-03-10 02:12:39 -0600

    updates to post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 # Force script to be run as sudo
 if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run as root, use sudo ./"$0" instead" 1>&2
-	exit 1
+    echo "This script must be run as root, use sudo ./"$0" instead" 1>&2
+    exit 1
 fi
 
+# the list of software to be installed via package manager
 SOFTWARE="vim git screenfetch"
 
 # check the operating system
 if [ -f /etc/os-release ]; then
-	source /etc/os-release
-	OS=$NAME
+    source /etc/os-release
+    OS=$ID
 else
-	echo "ERROR: Not supported operating system."
-	exit 1
+    echo "ERROR: Not supported operating system."
+    exit 1
 fi
 
 echo $OS
 
-if [ $OS == "Ubuntu" ]; then
-	INSTALLCMD="apt install -y"
+if [ "$OS" == "ubuntu" ] || [ "$OS" == "debian" ]; then
+    PKGMAN="apt install -y"
+elif [ "$OS" == "fedora" ] || [ "$OS" == "rhel" ]; then
+    PKGMAN="dnf install -y"
+else
+    echo "ERROR: Not supported operating system."
+    exit 1
 fi
 
-$INSTALLCMD $SOFTWARE
+# DEBUG
+echo $PKGMAN
+# UNCOMMENT NEXT LINE WHEN COMPLETE
+# $PKGMAN $SOFTWARE
+
